3f0276b4c913c667a2ba88652aeeeff88d224d6b
Tavish Negi <64775672+tavilefty@users.noreply.github.com>
2024-07-19 21:08:14 +0530

    init edit

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,11 @@ from logging.handlers import RotatingFileHandler
 import os
 from flask_mail import Mail
 from flask_moment import Moment
+from flask import request
+from flask_babel import Babel
+
+def get_locale():
+	return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,6 +23,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 moment = Moment(app)
+babel = Babel(app, locale_selector = get_locale)
 
 from app import routes, models, errors
 
